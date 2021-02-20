d346110586f8bbe2e8aae85960062bfc56b4cb65
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2021-02-20 23:12:03 +0200

    fix: api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BackupController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('backups', 'BackupController@index');
-Route::get('backups/logs', 'BackupController@logs');
+Route::get('backups', [BackupController::class, 'index']);
+Route::get('backups/logs', [BackupController::class, 'logs']);
