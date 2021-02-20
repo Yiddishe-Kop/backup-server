edf89c8f75dc3c4f9dae55a26c00435d4b91df3d
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2021-02-20 23:07:45 +0200

    feat: get backup data API

---
+++ app/Http/Controllers/BackupController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\BackupServer\Models\Backup;
+use Spatie\BackupServer\Models\BackupLogItem;
+
+class BackupController extends Controller
+{
+    public function index()
+    {
+        return Backup::paginate();
+    }
+
+    public function logs()
+    {
+        return BackupLogItem::paginate();
+    }
+}
