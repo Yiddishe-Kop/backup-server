c3cc984a003a1008e9d2d96e0aa71345579be558
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2021-03-07 00:18:08 +0200

    fix: increase max storage

--- app/Http/Controllers/BackupController.php
+++ app/Http/Controllers/BackupController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Spatie\BackupServer\Models\Backup;
-use Spatie\BackupServer\Models\BackupLogItem;
 
 class BackupController extends Controller
 {
